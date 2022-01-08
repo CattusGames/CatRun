14a6c1816d8ada421b12e156e06379e80dfd9414
CattusGames <cattusgames@gmail.com>
2022-01-09 00:18:59 +0300

    New cat model, optimize trajectory render

--- Assets/Scripts/TrajectoryRenderer.cs
+++ Assets/Scripts/TrajectoryRenderer.cs
@@ -5,21 +5,22 @@ using UnityEngine;
 public class TrajectoryRenderer : MonoBehaviour
 {
     private LineRenderer _lineRenderer;
-
+    [SerializeField]private Transform _player;
+    
     private void Start()
     {
+        
         _lineRenderer = gameObject.GetComponent<LineRenderer>();
     }
 
     public void ShowTrajectory(Vector3 origin, Vector3 speed)
     {
-        Vector3[] points = new Vector3[100];
+        Vector3[] points = new Vector3[10];
        
         for (int i = 0; i < points.Length; i++)
         {
             float time = i*0.1f;
             points[i] = origin + speed * time + Physics.gravity * time * time / 2f;
-        
         }
         _lineRenderer.positionCount = points.Length;
 
