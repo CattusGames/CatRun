b221e44cee0b7eb27e29e0649a7af97853637c29
CattusGames <67207327+CattusGames@users.noreply.github.com>
2022-07-25 00:06:29 +0300

    backup

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -55,7 +55,7 @@ public class ScoreManager : MonoBehaviour
     }
     public void ExitFromGPGS()
     {
-        PlayGamesPlatform.Instance.SignOut();
+    
     }
     public void IncrementScore()
     {
